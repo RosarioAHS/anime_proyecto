95c38622fa549a34727fb9febffb48fe146c25fe
Rosario Herrera <rosariiopochiherrera@gmail.com>
2026-02-02 20:01:03 +0100

    Creando el admin controller y sus respectivos html. Aun no comence con ningun html, solo los cree. Estoy con el controller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Peliculas;
+use App\Repository\PeliculasRepository;
+use App\Repository\RankingsRepository;
+use App\Repository\UsuariosRepository;
+use App\Repository\ValoracionesRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,23 +16,63 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/admin')]
 class AdminController extends AbstractController
 {
-    #[Route('/peliculas', name: 'admin_peliculas')]
-    public function peliculas(): Response
+    /**
+     * Dashboard principal del administrador
+     */
+    #[Route('/', name: 'admin_dashboard')]
+    public function dashboard(PeliculasRepository $peliculasRepository, UsuariosRepository $usuariosRepository, ValoracionesRepository $valoracionesRepository, RankingsRepository $rankingsRepository
+    ): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
-        return $this->render('admin/peliculas.html.twig', [
-            'message' => 'Próximamente: Gestión de Películas'
+        // Estadísticas básicas
+        $totalPeliculas = $peliculasRepository->count([]);
+        $totalUsuarios = $usuariosRepository->count([]);
+        $totalValoraciones = $valoracionesRepository->count([]);
+        $totalRankings = $rankingsRepository->count([]);
+
+        return $this->render('admin/dashboard.html.twig', [
+            'total_peliculas' => $totalPeliculas,
+            'total_usuarios' => $totalUsuarios,
+            'total_valoraciones' => $totalValoraciones,
+            'total_rankings' => $totalRankings,
         ]);
     }
 
-    #[Route('/estadisticas', name: 'admin_estadisticas')]
-    public function estadisticas(): Response
-    {
+    /**
+     * Gestión de peliculas (un CRUD)
+     */
+    #[Route('peliculas', name: 'admin_peliculas')]
+    public function peliculas(PeliculasRepository $peliculasRepository): Response{
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $peliculas = $peliculasRepository->findAll();
+        return $this->render('admin/peliculas.html.twig', ['peliculas' => $peliculas]);
+    }
+
+    /**
+     * Crear peli nueva de manera manual
+     */
+    #[Route('/peliculas/nueva', name: 'admin_peliculas_nueva', methods: ['GET', 'POST'])]
+    public function nuevaPelicula(Request, $request, EntityManagerInterface $entityManager): Response{
+
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
-        return $this->render('admin/estadisticas.html.twig', [
-            'message' => 'Próximamente: Estadísticas'
+        $pelicula = new Peliculas();
+        $form = $this->createForm(PeliculaFormType::class, $pelicula);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $pelicula->setGhibliId('manual-'.uniqid());
+            $entityManager->persist($pelicula);
+            $entityManager->flush();
+
+            $this->addFlash('Exitoso', 'La pelicula ha sido creada exitosamente.');
+            return $this->redirectToRoute('admin_peliculas');
+        }
+
+        return $this->render('admin/peliculas_form.html.twig', [
+            'form' => $form,
+            'pelicula' => null,
         ]);
     }
 }
---
+++ src/Form/PeliculaFormType.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PeliculaFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+    }
+}
