f6bdc27485fd0c29e5b0b4271967853f3b4902e0
Rosario Herrera <rosariiopochiherrera@gmail.com>
2026-02-16 23:22:18 +0100

    Ya si me funcionan las valoraciones al 100% y me parece que estaria todo listo. Solo tengo que mirar si esta todo correcto y cumple los requisitos pedido para el proyecto

--- src/Controller/ValoracionesController.php
+++ src/Controller/ValoracionesController.php
@@ -16,9 +16,6 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/valoraciones')]
 class ValoracionesController extends AbstractController
 {
-    /**
-     * Crear nueva valoración para una película
-     */
     #[Route('/crear/{peliculaId}', name: 'valoraciones_crear', methods: ['GET', 'POST'])]
     public function crear(
         int $peliculaId,
@@ -29,13 +26,11 @@ class ValoracionesController extends AbstractController
     ): Response {
         $this->denyAccessUnlessGranted('ROLE_USER');
 
-        // Buscar la película
         $pelicula = $peliculasRepository->find($peliculaId);
         if (!$pelicula) {
             throw $this->createNotFoundException('Película no encontrada');
         }
 
-        // Verificar si el usuario ya valoró esta película
         $valoracionExistente = $valoracionesRepository->findOneBy([
             'pelicula' => $pelicula,
             'usuario' => $this->getUser()
@@ -46,7 +41,6 @@ class ValoracionesController extends AbstractController
             return $this->redirectToRoute('valoraciones_editar', ['id' => $valoracionExistente->getId()]);
         }
 
-        // Crear nueva valoración
         $valoracion = new Valoraciones();
         $valoracion->setPelicula($pelicula);
         $valoracion->setUsuario($this->getUser());
@@ -58,6 +52,9 @@ class ValoracionesController extends AbstractController
             $entityManager->persist($valoracion);
             $entityManager->flush();
 
+            // Actualizar estadísticas
+            $this->actualizarEstadisticasPelicula($pelicula, $entityManager);
+
             $this->addFlash('success', '¡Valoración guardada exitosamente!');
             return $this->redirectToRoute('peliculas_show', ['id' => $pelicula->getId()]);
         }
@@ -68,9 +65,6 @@ class ValoracionesController extends AbstractController
         ]);
     }
 
-    /**
-     * Editar una valoración existente
-     */
     #[Route('/editar/{id}', name: 'valoraciones_editar', methods: ['GET', 'POST'])]
     public function editar(
         Valoraciones $valoracion,
@@ -79,7 +73,6 @@ class ValoracionesController extends AbstractController
     ): Response {
         $this->denyAccessUnlessGranted('ROLE_USER');
 
-        // Verificar que la valoración pertenece al usuario actual
         if ($valoracion->getUsuario() !== $this->getUser()) {
             throw $this->createAccessDeniedException('No puedes editar esta valoración');
         }
@@ -91,6 +84,9 @@ class ValoracionesController extends AbstractController
             $valoracion->setActualizadoEn(new \DateTimeImmutable());
             $entityManager->flush();
 
+            // Actualizar estadísticas
+            $this->actualizarEstadisticasPelicula($valoracion->getPelicula(), $entityManager);
+
             $this->addFlash('success', '¡Valoración actualizada exitosamente!');
             return $this->redirectToRoute('peliculas_show', ['id' => $valoracion->getPelicula()->getId()]);
         }
@@ -101,9 +97,6 @@ class ValoracionesController extends AbstractController
         ]);
     }
 
-    /**
-     * Eliminar una valoración
-     */
     #[Route('/eliminar/{id}', name: 'valoraciones_eliminar', methods: ['POST'])]
     public function eliminar(
         Valoraciones $valoracion,
@@ -112,17 +105,19 @@ class ValoracionesController extends AbstractController
     ): Response {
         $this->denyAccessUnlessGranted('ROLE_USER');
 
-        // Verificar que la valoración pertenece al usuario actual
         if ($valoracion->getUsuario() !== $this->getUser()) {
             throw $this->createAccessDeniedException('No puedes eliminar esta valoración');
         }
 
-        // Verificar token CSRF
         if ($this->isCsrfTokenValid('delete' . $valoracion->getId(), $request->request->get('_token'))) {
             $peliculaId = $valoracion->getPelicula()->getId();
+            $pelicula = $valoracion->getPelicula();
 
             $entityManager->remove($valoracion);
             $entityManager->flush();
+
+            // Actualizar estadísticas
+            $this->actualizarEstadisticasPelicula($pelicula, $entityManager);
 
             $this->addFlash('success', 'Valoración eliminada correctamente');
             return $this->redirectToRoute('peliculas_show', ['id' => $peliculaId]);
@@ -132,9 +127,6 @@ class ValoracionesController extends AbstractController
         return $this->redirectToRoute('peliculas_show', ['id' => $valoracion->getPelicula()->getId()]);
     }
 
-    /**
-     * Ver todas mis valoraciones
-     */
     #[Route('/mis-valoraciones', name: 'valoraciones_mis_valoraciones', methods: ['GET'])]
     public function misValoraciones(ValoracionesRepository $valoracionesRepository): Response
     {
@@ -148,5 +140,31 @@ class ValoracionesController extends AbstractController
         return $this->render('valoraciones/mis_valoraciones.html.twig', [
             'valoraciones' => $valoraciones,
         ]);
+    }
+
+    /**
+     * Actualizar contadorRating y promedioRating de una película
+     */
+    private function actualizarEstadisticasPelicula(Peliculas $pelicula, EntityManagerInterface $entityManager): void
+    {
+        $connection = $entityManager->getConnection();
+        $peliculaId = $pelicula->getId();
+
+        // Tengo que usar wl SQL porque de otra manera no se me actualizan las películas sino
+        $sql = "
+        UPDATE peliculas p
+        SET
+            p.contador_rating = (
+                SELECT COUNT(*)
+                FROM valoraciones v
+                WHERE v.id_pelicula = :peliculaId),
+            p.promedio_rating = (
+                SELECT COALESCE(AVG(CAST(v.puntuacion AS DECIMAL(10,2))), 0)
+                FROM valoraciones v
+                WHERE v.id_pelicula = :peliculaId)
+                WHERE p.id = :peliculaId
+    ";
+
+        $connection->executeStatement($sql, ['peliculaId' => $peliculaId]);
     }
 }
